c4628b37a041593ccf0dfc26dab666cfc2f6c898
Centurion195 <Vadim-0211@yandex.ru>
2023-02-03 18:57:46 +0300

    Task #1 completed

--- #1/Program.cs
+++ #1/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Найти кубы чисел от 1 до N
+
+Console.WriteLine("Введите число:");
+int number = int.Parse(Console.ReadLine() ?? "0");
+
+for (int i = 1; i<=number; i++) {
+    Console.WriteLine($"{i}^3={i*i*i}");
+}
