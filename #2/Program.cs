6cbd3f3174d2760cfe6f63d26322c38ee906e5ab
Centurion195 <Vadim-0211@yandex.ru>
2023-02-03 20:32:13 +0300

    Task #2

---
+++ #2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Подсчитать сумму цифр в числе
+
+Console.WriteLine("Введите число:");
+int number = Math.Abs(int.Parse(Console.ReadLine() ?? "0"));
+
+int res = 0;
+
+while (number!=0) {
+    res = number%10+res;
+    number = number/10;
+}
+
+Console.WriteLine($"Сумма цифр в числе: {res}");
