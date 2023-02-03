323574ff5b16e40ca21a6c3184e5565af1424e61
Centurion195 <Vadim-0211@yandex.ru>
2023-02-03 20:59:29 +0300

    Task #4

---
+++ #4/Program.cs
@@ -0,0 +1,10 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+
+Console.WriteLine("Введите число:");
+int number = int.Parse(Console.ReadLine() ?? "0");
+
+for (int i = 1; i<=number; i++) {
+    if (i%2==0) {
+        Console.WriteLine($"{i}^3={i*i*i}");
+    }
+}
